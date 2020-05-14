10263ac42a0c73d8d9eff9585d1ea3d0684a1637
github username <github email address>
2020-05-14 12:30:23 -0600

    Add code comments

--- app.rb
+++ app.rb
@@ -2,32 +2,27 @@ require('json')
 file = File.read('example_data_candidate.json')
 parsed_file = JSON.parse(file)
 
-
 def depth_first_search(source_index, data_hash, parsed_file)
 
-    node_stack = [source_index] #to work off a path 
-    visited = [] #to keep track of already visited accounts
-    path = [] #tokeep track of correct path 
+    node_stack = [source_index] 
+    visited = []
+    path = []  
     
-  
     loop do 
         curr_node = node_stack.pop
-
-        if path[0] == curr_node #check that node is source_index 
+        if path[0] == curr_node  
             path.push(curr_node)
             break
         end  
 
-        if (visited.include?(curr_node) or path.include?(curr_node)) && curr_node != path[0] #repeat of account that is not sourceindex  
+        if (visited.include?(curr_node) or path.include?(curr_node)) && curr_node != path[0]  
             break 
         end 
 
-        if data_hash[curr_node].empty? #to account for a node with no children 
+        if data_hash[curr_node].empty? 
             visited.push(curr_node)
             break 
         end 
-
-#problem is keeping track of siblings that are not correct path 
       
         path.push(curr_node)
         
@@ -36,55 +31,52 @@ def depth_first_search(source_index, data_hash, parsed_file)
             array.push(transfer["receivingAccountId"].to_i)
         end 
 
-    node_stack = (node_stack << array).flatten!
-     
+        node_stack = (node_stack << array).flatten!   
     end
+    return path 
 end
 
+def finding_fraud(arrays, parsed_file, data_hash)
 
-def finding_fraud(parsed_file, final_arrays, data_hash)
-
-    # money_hash = {} 
-    # parsed_file.each do |account|
-    #     money_hash[account["accountId"]] = account["outboundTransferTotals"]["quantity"]   #make data_hash instead 
-    # end 
-
+    account_array = arrays.flatten(1)
 
     final_object = {}
-    final_arrays.each_with_index do |array, index|
+    account_array.each_with_index do |array, index|
         if array.length < 4
-            final_arrays.delete_at(index)
+            account_array.delete_at(index)
         end 
 
         if array[-1] != array[0]
-            final_arrays.delete_at(index)
+            account_array.delete_at(index)
         end 
 
-        # array.reduce(0) do |sum, account|
-        #    sum += money_hash[account]
-        # end 
-
-        array.each_with_index do |account, index|
-            array.reduce(0) do |sum, account|
-               transfer_object = data_hash[account["accountid"]].find do |transfer|
-                    transfer["receivingAccountId"] = array[(index+1)]
-                end 
-            sum += transfer_object["quantity"]
-            end 
-        end     
-
-        if sum < 50000
-            final_arrays.delete_at(index)
+        final_sum = array.reduce(0) do |sum, account|
+            account_to_check = account 
+           sum += find_money_value(account_to_check, data_hash, array)
         end 
 
-        final_object[array] = sum      
+        if final_sum < 50000
+            account_array.delete_at(index)
+        end 
+
+        final_object[array] = final_sum      
     end 
-
-    final_object
-
+    p final_object
 end 
 
-def find_paths(parsed_file)
+def find_money_value(account_to_check, data_hash, array)
+    array.each_with_index do |account, index|
+        if account_to_check == account 
+          outbound_transfer = array[index + 1] 
+          transfer_object = data_hash[account].find do |transfer|
+            transfer["receivingAccountId"] = outbound_transfer
+          end 
+        end
+        return transfer_object["quantity"]
+    end 
+end 
+        
+def iterate_through_accounts(parsed_file)
     parsed_file.each_with_index do |account, index|
         if account["inboundTransfersTotals"].empty? && account["outboundTransferTotals"].empty? 
           parsed_file.delete_at(index)
@@ -95,51 +87,17 @@ def find_paths(parsed_file)
     parsed_file.each do |account| 
         data_hash[account["accountId"]] = account["outboundTransferTotals"]
     end 
+
     arrays = []
     parsed_file.each do |account|
         source_index = account["accountId"] 
         arrays.push(depth_first_search(source_index, data_hash, parsed_file))
     end 
-    final_arrays = arrays.flatten!
-    finding_fraud(final_arrays, parsed_file, data_hash)
+
+    finding_fraud(arrays, parsed_file, data_hash)
 end 
 
-find_paths(parsed_file)
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 1. Mark the current node as visited(initially current node is the root node)
-# 2. Check if current node is the goal, If so, then return it.
-# 3. Iterate over children nodes of current node, and do the following:
-#     1. Check if a child node is not visited.
-#     2. If so, then, mark it as visited.
-#     3. Go to it's sub tree recursively until you find the goal node(In other words, do the same steps here passing the child node as the current node in the next recursive call).
-#     4. If the child node has the goal node in this sub tree, then, return it.
-# 3. If goal node is not found, then goal node is not in the tree!
+iterate_through_accounts(parsed_file)
 
 
 
--- app2.rb
+++ app2.rb
@@ -5,19 +5,19 @@ parsed_file = JSON.parse(file)
 
 def make_path_arrays(source_index, data_hash, parsed_file)
     
-    node_stack = [source_index]
-    visited = []
-    path = [] 
+    node_stack = [source_index] #to work off a path 
+    visited = [] #to keep track of already visited accounts
+    path = [] #to keep track of correct path
     
     loop do 
       curr_node = node_stack.pop
-        if visited.include?(curr_node) 
-            return path.push(visited)
+        if visited.include?(curr_node) #to check if the loop has reached the origin account
+            return path.push(visited) #if it has, push visited array into path array
         end 
 
         visited.push(curr_node)
 
-        array = []
+        array = [] #array of child accounts
         data_hash[curr_node].each do |transfer| 
             array.push(transfer["receivingAccountId"].to_i)
         end 
@@ -25,44 +25,21 @@ def make_path_arrays(source_index, data_hash, parsed_file)
  
         node_stack = (node_stack << array).flatten!
     end 
-    return path
+    return path #method returns the correct circular payment path
 end 
-
-def iterate_through_accounts(parsed_file)
-    parsed_file.each_with_index do |account, index|
-        if account["inboundTransfersTotals"].empty? && account["outboundTransferTotals"].empty? 
-            parsed_file.delete_at(index)
-        end 
-    end 
-
-    data_hash = {} 
-    parsed_file.each do |account| 
-        data_hash[account["accountId"]] = account["outboundTransferTotals"]
-    end 
-
-    arrays = []
-    parsed_file.each do |account|
-        source_index = account["accountId"] 
-       arrays.push(make_path_arrays(source_index, data_hash, parsed_file))
-    end 
-    arrays 
-    
-    finding_fraud(arrays, parsed_file)
-end 
-    
 
 def finding_fraud(arrays, parsed_file)
 
    account_array = arrays.flatten(1) #necessary to flatten the nested arrays we have made
 
-    money_hash = {} 
+    money_hash = {} #creates hash/object with key of account-id and value of outbound transfer quantity 
     parsed_file.each do |account|
        money_hash[account["accountId"]] = account["outboundTransferTotals"][0]["quantity"]   
     end 
 
-    final_object = {} #the object we will return with key of account and value of money total
+    final_object = {} 
     account_array.uniq.each_with_index do |array, index|
-        if array.length < 4
+        if array.length < 3 #if less than 3, does not qualify (the last account is not included) 
             account_array.delete_at(index)
             break
         end 
@@ -71,17 +48,39 @@ def finding_fraud(arrays, parsed_file)
             sum += money_hash[account]
         end 
 
-        if final_sum < 50000
+        if final_sum < 50000 #if less than 50000, does not qualify 
             account_array.delete_at(index)
             break 
         end 
 
-        final_object[array] = final_sum      
+        final_object[array] = final_sum #creates hash/object with key of account and value of money total  
     end 
 
-   p final_object
+    p final_object #prints final object! 
 
 end 
 
-iterate_through_accounts(parsed_file)
+def iterate_through_accounts(parsed_file)
+    parsed_file.each_with_index do |account, index| #removes accounts without outbound & inbound transfers
+        if account["inboundTransfersTotals"].empty? && account["outboundTransferTotals"].empty? 
+            parsed_file.delete_at(index)
+        end 
+    end 
+
+    data_hash = {} #creates hash with key of account-id and value of outbound transfer object
+    parsed_file.each do |account| 
+        data_hash[account["accountId"]] = account["outboundTransferTotals"]
+    end 
+
+    arrays = []
+    parsed_file.each do |account| #iterate through each account to find all possible paths
+        source_index = account["accountId"] 
+       arrays.push(make_path_arrays(source_index, data_hash, parsed_file)) #push resulting path array of make_path_arrays method into final array
+    end 
+    arrays 
+    
+    finding_fraud(arrays, parsed_file) #now to check if the array of path arrays are shady 
+end 
+
+iterate_through_accounts(parsed_file) #call the this method to cycle through all other methods 
       
